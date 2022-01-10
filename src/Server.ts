71c7806ae37c8fa6280b8a44c7c0f17681afcd9f
Suares01 <hub_suares.01@hotmail.com>
2022-01-10 19:55:02 -0300

    Initial development of forecast route

---
+++ src/Server.ts
@@ -0,0 +1,27 @@
+import { Server } from '@overnightjs/core';
+import bodyParser from 'body-parser';
+
+import { ForecastController } from './controllers/ForecastController';
+
+export class SetupServer extends Server {
+    constructor(private port = process.env.SERVER_PORT) {
+        super();
+    }
+
+    public initServer(): void {
+        this.setupExpress();
+        this.setupControllers();
+    }
+
+    private setupExpress(): void {
+        this.app.use(bodyParser.json());
+    }
+
+    private setupControllers(): void {
+        const forecastController = new ForecastController();
+
+        this.addControllers([
+            forecastController,
+        ]);
+    }
+}
