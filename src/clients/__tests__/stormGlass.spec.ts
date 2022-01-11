be95e5e6563483177f22e09593b87cbc0d84fb12
Suares01 <hub_suares.01@hotmail.com>
2022-01-11 18:40:48 -0300

    storm glass tests

---
+++ src/clients/__tests__/stormGlass.spec.ts
@@ -0,0 +1,21 @@
+import { StormGlass } from '@src/clients/StormGlass';
+import stormGlassNormalizedWeatherFixtures from '@tests/fixtures/stormGlassNormalizedWeather15hoursFixtues.json';
+import stormGlassWeatherFixtures from '@tests/fixtures/stormGlassWeather15hoursFixtures.json';
+import axios from 'axios';
+
+jest.mock('axios');
+
+describe('Storm Glass client', () => {
+  it('should return the normalized forecast from the StormGlass service', async () => {
+    const lat = -22.9461;
+    const lng = -43.1811;
+
+    axios.get = jest.fn().mockResolvedValue(stormGlassWeatherFixtures);
+
+    const stormGlass = new StormGlass(axios);
+
+    const response = await stormGlass.fetchPoints(lat, lng);
+
+    expect(response).toEqual(stormGlassNormalizedWeatherFixtures);
+  });
+});
