87493ffebe6c1732f1889d856dd5f66752b26324
Suares01 <hub_suares.01@hotmail.com>
2022-01-15 19:20:18 -0300

    Add more generic types

--- src/util/Request.ts
+++ src/util/Request.ts
@@ -1,18 +1,21 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 
-export type IRequestConfig = AxiosRequestConfig
+export type RequestConfig<D = any> = AxiosRequestConfig<D>
 
-export type Response<T = any> = AxiosResponse<T>
+export type Response<T = any, D = any> = AxiosResponse<T, D>
 
 export class Request {
   constructor(private request = axios) {}
 
-  public get<T>(url: string, config: IRequestConfig = {}): Promise<Response<T>> {
-    return this.request.get<T, Response<T>>(url, config);
+  public get
+  <T = any, R = Response<T>, D = any>(
+    url: string,
+    config: RequestConfig<D> = {},
+  ): Promise<R> {
+    return this.request.get<T, R>(url, config);
   }
 
-  public static isRequestError(error: AxiosError): boolean {
+  public static isRequestError<T = any, D = any>(error: AxiosError<T, D>): boolean {
     return !!(error.response && error.response.status);
   }
 }
