67fdd57f9f09411bbc8f8c9db2d3cc3a2e41ebac
Suares01 <hub_suares.01@hotmail.com>
2022-01-15 14:44:04 -0300

    Add StormGlassRequestError

---
+++ src/util/errors/StormGlassRequestError.ts
@@ -0,0 +1,8 @@
+import { InternalError } from './InternalError';
+
+export class StormGlassRequestError extends InternalError {
+  constructor(message: string) {
+    const internalMessage = 'Unexpected error returned by the StormGlass service';
+    super(`${internalMessage}: ${message}`);
+  }
+}
