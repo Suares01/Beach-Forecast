24dba74386d8e673c726873561149ee59a88f076
Suares01 <hub_suares.01@hotmail.com>
2022-01-17 18:05:11 -0300

    Adjust send response value

--- src/controllers/ForecastController.ts
+++ src/controllers/ForecastController.ts
@@ -5,39 +5,27 @@ import { Request, Response } from 'express';
 export class ForecastController {
   @Get('')
   public getForecastForLoggedUser(_: Request, res: Response): void {
-    res.send([{
-      time: '2020-04-26T00:00:00+00:00',
-      forecast: [{
-        lat: -33.792726,
-        lng: 151.289824,
-        name: 'Manly',
-        position: 'E',
-        rating: 2,
-        swellDirection: 64.26,
-        swellHeight: 0.15,
-        swellPeriod: 3.89,
-        time: '2020-04-26T00:00:00+00:00',
-        waveDirection: 231.38,
-        waveHeight: 0.47,
-        windDirection: 299.45,
-      }],
-    }, {
-      time: '2020-04-26T01:00:00+00:00',
-      forecast: [{
-        lat: -33.792726,
-        lng: 151.289824,
-        name: 'Manly',
-        position: 'E',
-        rating: 2,
-        swellDirection: 123.41,
-        swellHeight: 0.21,
-        swellPeriod: 3.67,
-        time: '2020-04-26T01:00:00+00:00',
-        waveDirection: 232.12,
-        waveHeight: 0.46,
-        windDirection: 310.48,
-      }],
-    },
+    res.send([
+      {
+        time: '2022-01-11T18:00:00+00:00',
+        forecast: [
+          {
+            lat: -22.9461,
+            lng: -43.1811,
+            name: 'Copacabana',
+            position: 'E',
+            rating: 1,
+            swellDirection: 156.73,
+            swellHeight: 0.72,
+            swellPeriod: 8.36,
+            time: '2022-01-11T18:00:00+00:00',
+            waveDirection: 153.44,
+            waveHeight: 0.72,
+            windDirection: 130.65,
+            windSpeed: 3.0,
+          },
+        ],
+      },
     ]);
   }
 }
