102f256f864d1cba49f53b3e9a7dffb95fccb13c
Suares01 <hub_suares.01@hotmail.com>
2022-01-19 11:44:23 -0300

    Change uri's

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,13 +1,18 @@
-import { IDatabase } from '@config/types/configTypes';
-import config from 'config';
 import mongoose, { Mongoose } from 'mongoose';
 
-const dbConfig = config.get<IDatabase>('App.database');
+const {
+  DB_USER, DB_PASS, DB_NAME,
+} = process.env;
+
+// const uriProd = `mongodb+srv://${DB_USER}:${DB_PASS}@surfapi.h3qnx.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
+const uriDev = `mongodb://${DB_USER}:${DB_PASS}@localhost:27017/${DB_NAME}?authSource=admin`;
 
 export const connect = async (): Promise<Mongoose> => {
-  mongoose.connection.on('error', () => console.error('Database connection error'));
-  mongoose.connection.on('open', () => console.log('Database conected'));
-  return mongoose.connect(dbConfig.mongoUrl);
+  mongoose.connection.on('error', () => console.error('Database connection failed'));
+  mongoose.connection.once('open', () => console.log('Database connected'));
+
+  return mongoose.connect(uriDev);
 };
 
 export const close = (): Promise<void> => mongoose.connection.close();
