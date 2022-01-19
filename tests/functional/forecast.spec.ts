d0f5cf7b3dd6d4539d52a113644a5150c80585a4
Suares01 <hub_suares.01@hotmail.com>
2022-01-19 17:25:04 -0300

    Implement Forecast tests

--- tests/functional/forecast.spec.ts
+++ tests/functional/forecast.spec.ts
@@ -1,30 +1,56 @@
+import { Beach, BeachPosition } from '@src/models/Beach';
+import apiForecastResponse from '@tests/fixtures/apiForecastResponse.json';
+import stormGlassWeather15hoursFixtures from '@tests/fixtures/stormGlassWeather15hoursFixtures.json';
+import nock from 'nock';
+
 describe('Beach forecast functional tests', () => {
+  beforeEach(async () => {
+    await Beach.deleteMany();
+
+    const defaultBeach = {
+      lat: -22.9461,
+      lng: -43.1811,
+      name: 'Copacabana',
+      position: BeachPosition.east,
+    };
+
+    const beach = new Beach(defaultBeach);
+
+    await beach.save();
+  });
+
+  const nockInterceptor = nock('https://api.stormglass.io', {
+    encodedQueryParams: true,
+    reqheaders: {
+      Authorization: (): boolean => true,
+    },
+  })
+    .get('/v2/weather/point')
+    .query({
+      lat: -22.9461,
+      lng: -43.1811,
+      params: 'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed',
+      source: 'noaa',
+      start: 1641924000,
+      end: 1641924000,
+    });
+
   it('should return a forecast with just a few times', async () => {
+    nockInterceptor
+      .reply(200, stormGlassWeather15hoursFixtures);
+
     const { body, status } = await global.testRequest.get('/forecast');
+
     expect(status).toBe(200);
-    expect(body).toEqual(
-      [
-        {
-          time: '2022-01-11T18:00:00+00:00',
-          forecast: [
-            {
-              lat: -22.9461,
-              lng: -43.1811,
-              name: 'Copacabana',
-              position: 'E',
-              rating: 1,
-              swellDirection: 156.73,
-              swellHeight: 0.72,
-              swellPeriod: 8.36,
-              time: '2022-01-11T18:00:00+00:00',
-              waveDirection: 153.44,
-              waveHeight: 0.72,
-              windDirection: 130.65,
-              windSpeed: 3.0,
-            },
-          ],
-        },
-      ],
-    );
+    expect(body).toEqual(apiForecastResponse);
+  });
+
+  it('should return 500 if something goes wrong during the processing', async () => {
+    nockInterceptor
+      .replyWithError('Something went wrong');
+
+    const { status } = await global.testRequest.get('/forecast');
+
+    expect(status).toBe(500);
   });
 });
