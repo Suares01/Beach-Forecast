2c6629d9bfa1133afcc0aa1644bcb142a3bda47c
Suares01 <hub_suares.01@hotmail.com>
2022-01-19 17:23:20 -0300

    Implement Forecast service and Beach model

--- src/controllers/ForecastController.ts
+++ src/controllers/ForecastController.ts
@@ -1,31 +1,22 @@
 import { Controller, Get } from '@overnightjs/core';
+import { Beach } from '@src/models/Beach';
+import { Forecast } from '@src/services/Forecast';
 import { Request, Response } from 'express';
 
 @Controller('forecast')
 export class ForecastController {
   @Get('')
-  public getForecastForLoggedUser(_: Request, res: Response): void {
-    res.send([
-      {
-        time: '2022-01-11T18:00:00+00:00',
-        forecast: [
-          {
-            lat: -22.9461,
-            lng: -43.1811,
-            name: 'Copacabana',
-            position: 'E',
-            rating: 1,
-            swellDirection: 156.73,
-            swellHeight: 0.72,
-            swellPeriod: 8.36,
-            time: '2022-01-11T18:00:00+00:00',
-            waveDirection: 153.44,
-            waveHeight: 0.72,
-            windDirection: 130.65,
-            windSpeed: 3.0,
-          },
-        ],
-      },
-    ]);
+  public async getForecastForLoggedUser(_: Request, res: Response): Promise<Response> {
+    try {
+      const forecast = new Forecast();
+
+      const beaches = await Beach.find({});
+
+      const forecasts = await forecast.processForecastForBeaches(beaches);
+
+      return res.status(200).send(forecasts);
+    } catch (err) {
+      return res.status(500).send({ error: 'Something went wrong' });
+    }
   }
 }
