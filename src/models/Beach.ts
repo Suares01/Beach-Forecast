33f6710b62baf9d854e1d029c776065b9154b86b
Suares01 <hub_suares.01@hotmail.com>
2022-01-19 11:44:45 -0300

    Create beach model

---
+++ src/models/Beach.ts
@@ -0,0 +1,38 @@
+import mongoose, { Document, Model } from 'mongoose';
+
+export enum BeachPosition {
+  south = 'S',
+  east = 'E',
+  west = 'W',
+  north = 'N'
+}
+
+export interface IBeach {
+  _id?: string
+  lat: number
+  lng: number
+  name: string
+  position: BeachPosition
+}
+
+interface IBeachModel extends Omit<IBeach, '_id'>, Document {}
+
+const schema = new mongoose.Schema(
+  {
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+    name: { type: String, required: true },
+    position: { type: String, required: true },
+  },
+  {
+    toJSON: {
+      transform: (_, ret): void => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  },
+);
+
+export const Beach: Model<IBeachModel> = mongoose.model('Beach', schema);
