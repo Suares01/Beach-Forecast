ec92e7fc0f202abcaf7bf177f1433b968fd81228
Suares01 <hub_suares.01@hotmail.com>
2022-01-21 20:31:32 -0300

    Application bootstrap

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { SetupServer } from './Server';
+
+const server = new SetupServer();
+
+(async (): Promise<void> => {
+  await server.initServer();
+  server.start();
+})();
+
+process.once('SIGINT', async () => {
+  await server.close();
+});
