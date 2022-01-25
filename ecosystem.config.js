a564096f769bb257b34f9a812313cb4d78e2cddb
Suares01 <hub_suares.01@hotmail.com>
2022-01-25 17:43:13 -0300

    Change code style

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,18 @@
 module.exports = {
   apps: [
     {
-      name: 'surfApi',
-      script: './dist/index.js',
-      watch: './dist',
-      instance_var: 'INSTANCE_ID',
+      name: "surfApi",
+      script: "./dist/index.js",
+      watch: "./dist",
+      instance_var: "INSTANCE_ID",
       env: {
-        NODE_ENV: 'production',
+        NODE_ENV: "production",
       },
-      node_args: '-r dotenv/config',
-      ignore_watch: ['node_modules'],
-      args: '--no-daemon --watch',
+      node_args: "-r dotenv/config",
+      ignore_watch: ["node_modules"],
+      args: "--no-daemon --watch",
       wait_ready: true,
-      max_memory_restart: '400M',
+      max_memory_restart: "400M",
     },
   ],
 };
--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,13 @@
 export default {
-  displayName: 'root-tests',
-  testMatch: ['<rootDir>/src/**/*.spec.ts'],
-  testEnvironment: 'node',
+  displayName: "root-tests",
+  testMatch: ["<rootDir>/src/**/*.spec.ts"],
+  testEnvironment: "node",
   bail: true,
   clearMocks: true,
   moduleNameMapper: {
-    '@src/(.*)': '<rootDir>/src/$1',
-    '@tests/(.*)': '<rootDir>/tests/$1',
-    '@config/(.*)': '<rootDir>/config/$1',
+    "@src/(.*)": "<rootDir>/src/$1",
+    "@tests/(.*)": "<rootDir>/tests/$1",
+    "@config/(.*)": "<rootDir>/config/$1",
   },
-  preset: 'ts-jest',
+  preset: "ts-jest",
 };
