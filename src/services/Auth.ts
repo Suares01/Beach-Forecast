597ce373849ca0285742743a5d8836c4ef9d02c0
Suares01 <hub_suares.01@hotmail.com>
2022-01-25 17:42:17 -0300

    Add generateToken and decodeToken methods

--- src/services/Auth.ts
+++ src/services/Auth.ts
@@ -1,6 +1,17 @@
 import { compare, hash } from "bcrypt";
+import config from "config";
+import { sign, verify } from "jsonwebtoken";
+
+import { IAuth } from "@config/types/configTypes";
+import { IUser } from "@src/models/User";
+
+export interface IDecodedUser extends Omit<IUser, "_id"> {
+  id: string;
+}
 
 export class AuthService {
+  private static authConfig = config.get<IAuth>("App.auth");
+
   public static async hashPassword(
     password: string,
     salt = 10
@@ -17,5 +28,19 @@ export class AuthService {
     const comparePass = await compare(password, hashedPass);
 
     return comparePass;
+  }
+
+  public static generateToken(payload: object): string {
+    const token = sign(payload, this.authConfig.secret, {
+      expiresIn: "1d",
+    });
+
+    return token;
+  }
+
+  public static decodeToken(token: string): IDecodedUser {
+    const userInfo = verify(token, this.authConfig.secret) as IDecodedUser;
+
+    return userInfo;
   }
 }
