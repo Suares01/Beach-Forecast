542ffc9d06233a85d19cd631913901da609b96c6
Suares01 <hub_suares.01@hotmail.com>
2022-01-25 17:37:28 -0300

    Adjust test config

--- config/test.js
+++ config/test.js
@@ -1,15 +1,7 @@
-require("dotenv/config");
-
-const { DB_USER, DB_PASS, DB_NAME, DB_PORT } = process.env;
-
 module.exports = {
   App: {
-    database: {
-      uri: `mongodb://${DB_USER}:${DB_PASS}@localhost:${DB_PORT}/${DB_NAME}?authSource=admin`,
-    },
     resources: {
       StormGlass: {
-        endpoint: "https://api.stormglass.io/v2",
         apiKey: "test",
       },
     },
