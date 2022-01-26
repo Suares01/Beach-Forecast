c5239e3820bec5b4917714cf2e96a9af76580909
Suares01 <hub_suares.01@hotmail.com>
2022-01-25 23:07:31 -0300

    Add PORT env

--- config/production.js
+++ config/production.js
@@ -2,7 +2,7 @@ const { DB_USER, DB_PASS, DB_NAME } = process.env;
 
 module.exports = {
   App: {
-    port: 8080,
+    port: process.env.PORT,
     database: {
       uri: `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_NAME}.h3qnx.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
     },
