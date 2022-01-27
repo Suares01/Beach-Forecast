8480b5c7ce6c49139ab3aa49c57e4a2034c6f3a8
Suares01 <hub_suares.01@hotmail.com>
2022-01-26 22:00:27 -0300

    Adjust connect function

--- src/database/database.ts
+++ src/database/database.ts
@@ -6,20 +6,19 @@ import { SetupServer } from "@src/Server";
 
 const database = config.get<IDatabaseConfig>("App.database");
 
-export const connect = async (): Promise<Mongoose> => {
-  mongoose.connection.on("error", async () => {
-    const server = new SetupServer();
-    console.error("\x1b[31m%s\x1b[0m", "Database connection failed");
+mongoose.connection.on("error", async () => {
+  const server = new SetupServer();
+  console.error("\x1b[31m%s\x1b[0m", "Database connection failed");
 
-    await server.close();
-  });
+  await server.close();
+});
 
-  mongoose.connection.once("open", async () =>
-    console.log("\x1b[32m%s\x1b[0m", "Database connected")
-  );
+mongoose.connection.once("open", async () =>
+  console.log("\x1b[32m%s\x1b[0m", "Database connected")
+);
 
-  return mongoose.connect(database.uri);
-};
+export const connect = async (): Promise<Mongoose> =>
+  mongoose.connect(database.uri);
 
 export const close = (): Promise<void> => {
   console.log("\x1b[32m%s\x1b[0m", "Database disconnected");
