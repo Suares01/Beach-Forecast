b2ca4d0d3d57aad0968f6efb8f8dee66ac6249af
Suares01 <hub_suares.01@hotmail.com>
2022-01-27 13:25:33 -0300

    Remove DB_USER and DB_PASS

--- config/default.js
+++ config/default.js
@@ -1,12 +1,12 @@
 require("dotenv/config");
 
-const { DB_USER, DB_PASS, DB_NAME, API_KEY, TOKEN_SECRET } = process.env;
+const { DB_NAME, API_KEY, TOKEN_SECRET } = process.env;
 
 module.exports = {
   App: {
     port: 3000,
     database: {
-      uri: `mongodb://${DB_USER}:${DB_PASS}@localhost:27017/${DB_NAME}?authSource=admin`,
+      uri: `mongodb://localhost:27017/${DB_NAME}?authSource=admin`,
     },
     auth: {
       secret: `${TOKEN_SECRET}`,
--- config/production.js
+++ config/production.js
@@ -1,10 +1,10 @@
-const { DB_NAME, DB_USER, DB_PASS, PORT } = process.env;
+const { DB_NAME, PORT } = process.env;
 
 module.exports = {
   App: {
     port: PORT || 8080,
     database: {
-      uri: `mongodb+srv://${DB_USER}:${DB_PASS}@surfapi.h3qnx.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
+      uri: `mongodb+srv://surfapi.h3qnx.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
     },
   },
 };
