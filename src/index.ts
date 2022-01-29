7ebd50b0ef248e5531de4ee57c5a6dbe7beab871
Suares01 <hub_suares.01@hotmail.com>
2022-01-29 16:13:24 -0300

    Graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,48 @@
+import logger from "./log/logger";
 import { SetupServer } from "./Server";
+
+export enum ExitStatus {
+  Failure = 1,
+  Success = 0,
+}
 
 const server = new SetupServer();
 
-(async (): Promise<void> => {
-  await server.initServer();
-  server.start();
-})();
+const exitSignals: NodeJS.Signals[] = ["SIGINT", "SIGTERM", "SIGQUIT"];
 
-process.once("SIGINT", async () => {
-  await server.close();
-  process.exit(0);
+process.on("unhandledRejection", (reason, promise) => {
+  logger.error(
+    `App exiting due to an unhandled promsie: ${promise} ans reason: ${reason}`
+  );
+
+  throw reason;
 });
+
+process.on("uncaughtException", (error) => {
+  logger.error(`App exiting due to an uncaught exception: ${error}`);
+
+  process.exit(ExitStatus.Failure);
+});
+
+(async (): Promise<void> => {
+  try {
+    await server.initServer();
+    server.start();
+
+    exitSignals.map((signal) =>
+      process.on(signal, async () => {
+        try {
+          await server.close();
+          logger.info("App exited with success");
+          process.exit(ExitStatus.Success);
+        } catch (error) {
+          logger.error(`App exited with error: ${error}`);
+          process.exit(ExitStatus.Failure);
+        }
+      })
+    );
+  } catch (error) {
+    logger.error(`App exited with error: ${error}`);
+    process.exit(ExitStatus.Failure);
+  }
+})();
