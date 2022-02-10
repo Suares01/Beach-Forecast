03548dcbef513839686a6b00a35487372cb67c2e
Suares01 <hub_suares.01@hotmail.com>
2022-02-10 19:34:34 -0300

    Test getUnixTime method

---
+++ src/util/__tests__/time.spec.ts
@@ -0,0 +1,13 @@
+import { Time } from "../Time";
+
+describe("Time tests", () => {
+  it("should return the next 24 hours in unix timestamp", () => {
+    const now = Math.floor(Date.now() / 1000);
+    const next24Hours = now + 86400;
+
+    const time = Time.getUnixTime();
+
+    expect(time.start).toBe(now);
+    expect(time.end).toBe(next24Hours);
+  });
+});
