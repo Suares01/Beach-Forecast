2565624ddb135ece7055df732a5cb06dbbfbf569
Suares01 <hub_suares.01@hotmail.com>
2022-04-27 20:26:03 -0300

    throw new InternalError on mongoose error

--- src/database/database.ts
+++ src/database/database.ts
@@ -3,12 +3,13 @@ import mongoose, { Mongoose } from "mongoose";
 
 import { IDatabaseConfig } from "@config/types/configTypes";
 import logger from "@src/log/logger";
+import { InternalError } from "@src/util/errors/InternalError";
 
 const database = config.get<IDatabaseConfig>("App.database");
 
-mongoose.connection.on("error", (error) =>
-  logger.error(`Database error: ${error}`)
-);
+mongoose.connection.on("error", (error) => {
+  throw new InternalError(error.message);
+});
 
 mongoose.connection.on("open", () => logger.info("Database connected"));
 
@@ -17,7 +18,7 @@ mongoose.connection.on("disconnected", () =>
 );
 
 export const connect = async (): Promise<Mongoose> =>
-  await mongoose.connect(database.uri);
+  await mongoose.connect(database.url);
 
 export const close = async (): Promise<void> =>
   await mongoose.connection.close();
